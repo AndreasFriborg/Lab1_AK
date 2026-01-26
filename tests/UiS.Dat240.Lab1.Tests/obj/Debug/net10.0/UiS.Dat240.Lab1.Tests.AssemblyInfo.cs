0b5915132119461411c71b3453351aca2485a12c
KristianN55 <narten13@gmail.com>
2026-01-26 20:27:09 +0100

    merge?

--- tests/UiS.Dat240.Lab1.Tests/obj/Debug/net10.0/UiS.Dat240.Lab1.Tests.AssemblyInfo.cs
+++ tests/UiS.Dat240.Lab1.Tests/obj/Debug/net10.0/UiS.Dat240.Lab1.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UiS.Dat240.Lab1.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+984275066f23d9863b8330a65e5d845683cdda0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dcce0911901ee063a0d0623a02e5999ca79f8d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("UiS.Dat240.Lab1.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UiS.Dat240.Lab1.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
